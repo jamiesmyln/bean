cae69b6f8982ba4ac9a7fc72e42ea082bd7197b5
Smylnycky, Jamie <jamie.smylnycky@contractor.cengage.com>
2015-04-29 08:26:38 -0400

    bean progress

--- src/js/pet.js
+++ src/js/pet.js
@@ -1,7 +1,20 @@
-/* Pet needs:
-- health
-- happiness
-- hunger
-- emotions
-- movement
-*/
+var hunger = 0;
+
+function feed() {
+	if (hunger >= 0 && hunger <= 9) {
+		hunger++;
+		document.getElementById("hunger").style.width = hunger + "0%";
+		console.log(hunger);
+	}
+};
+
+function hungry() {
+	if (hunger >= 1 && hunger <= 10) {
+		hunger--;
+		document.getElementById("hunger").style.width = hunger + "0%";
+		console.log(hunger);
+	}
+};
+
+//food button click: add to fullness
+//hunger button click: subtract from fullness
